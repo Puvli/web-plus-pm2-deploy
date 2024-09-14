85b558957d98677ee0d7d3bdafe24a6f74608a55
Puvli44 <pavel-zajtsev@yandex.ru>
2024-09-14 23:54:27 +0300

    edited ecosystem.config.js in frontend

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
-const path = require('path');
+const path = require("path");
 // dotenv.config({ path: "./env.deploy" });
-dotenv.config({ path: path.resolve(__dirname, '.env.deploy') });
+dotenv.config({ path: path.resolve(__dirname, ".env.deploy") });
 
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } =
   process.env;
@@ -14,9 +14,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      "post-deploy":
-        "cd frontend && pwd && npm ci && npm run build",
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
     },
   },
 };
